a29b51db50a56388eb2c2397ce0c5f6a812e2c4d
Michuupk <michal.pakula3@gmail.com>
2025-01-21 08:27:51 +0100

    working sphere

--- main.py
+++ main.py
@@ -43,49 +43,60 @@ def startup():
     glClearColor(0.0, 0.0, 0.0, 1.0)
     update_viewport(None, 800, 800)
 
+    glEnable(GL_DEPTH_TEST)
+    glShadeModel(GL_SMOOTH)
 
 def sphere(center):
     global N, latitude, s, longitude, t, current_x, current_y, current_z, next_x, next_y, next_z
 
-    sphere_points = np.zeros((N, N, 3))
+    tab = np.zeros((N, N, 3))
+    texture = np.zeros((N, N, 2))
 
-    for longitude in np.linspace(0, 2 * np.pi, N):
-        for latitude in np.linspace(0, np.pi, N):
-            next_x = radius * math.cos(latitude) * math.sin(longitude) + center[0]
-            next_y = radius * math.sin(latitude) * math.sin(longitude) + center[1]
-            next_z = radius * math.cos(longitude) + center[2]
-            sphere_points[int(longitude), int(latitude)] = [next_x, next_y, next_z]
-            glColor3f(1.0, 1.0, 1.0)
-            glVertex3f(next_x, next_y, next_z)
-            glColor3f(1.0, 1.0, 0.0)
-            glVertex3f(current_x, current_y, current_z)
-            current_x = next_x
-            current_y = next_y
-            current_z = next_z
+    ul = np.linspace(0.0, 0.5, N)
+    vl = np.linspace(0.0, 2.0, N)
 
+    ut = np.linspace(0.0, 1.0, N)
+    vt = np.linspace(0.0, 1.0, N)
+
+    for j, u in enumerate(ul):
+        for i, v in enumerate(vl):
+            theta = 2 * math.pi * u  # azimuthal angle
+            phi = math.pi * v  # polar angle
+            x = math.sin(phi) * math.cos(theta)
+            y = math.sin(phi) * math.sin(theta)
+            z = math.cos(phi)
+            tab[i, j] = [x, y, z]
+            texture[j, i] = [ut[i], vt[j]]
+
+    glFrontFace(GL_CW)
     glBegin(GL_TRIANGLES)
-    for i in range(1, N):
-        for j in range(1, N):
-            v1 = sphere_points[i - 1, j - 1]
-            v2 = sphere_points[i, j - 1]
-            v3 = sphere_points[i, j]
-            v4 = sphere_points[i - 1, j]
+    for j in range(1, N):
+        for i in range(1, N):
+            v1 = tab[i - 1, j - 1]
+            v2 = tab[i, j - 1]
+            v3 = tab[i, j]
+            v4 = tab[i - 1, j]
+
+            glColor3f(1.0, 1.0, 1.0)  # white
+            #glNormal3f(*normal1)
+            glTexCoord2f(*texture[i - 1, j - 1])  # Współrzędne tekstury dla v1
+            glVertex3f(*v1)
+            glTexCoord2f(*texture[i, j - 1])  # Współrzędne tekstury dla v2
+            glVertex3f(*v2)
+            glTexCoord2f(*texture[i, j])  # Współrzędne tekstury dla v3
+            glVertex3f(*v3)
 
             # Drugi trójkąt
             glColor3f(1.0, 1.0, 1.0)  # white
+            #glNormal3f(*normal2)
+            glTexCoord2f(*texture[i - 1, j - 1])  # Współrzędne tekstury dla v1
             glVertex3f(*v1)
-            glVertex3f(*v2)
+            glTexCoord2f(*texture[i, j])  # Współrzędne tekstury dla v3
             glVertex3f(*v3)
-
-
-            glColor3f(1.0, 1.0, 0.0)
-            glVertex3f(*v1)
-            glVertex3f(*v3)
+            glTexCoord2f(*texture[i - 1, j])  # Współrzędne tekstury dla v4
             glVertex3f(*v4)
-        
-        
-
     glEnd()
+
 
 def mouse_motion_callback(window, x_pos, y_pos):
     global delta_x
